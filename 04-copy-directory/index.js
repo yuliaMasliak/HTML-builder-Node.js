3f4c2efcaaf4aa88aca7eb697b2cd6cf680305fa
Yulia Masliak <1985kozak@gmail.com>
2023-05-04 15:02:42 +0300

    refactor: remove statSync from 3 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,6 @@
+import { copyFile, constants } from 'node:fs/promises';
+const fs = require('fs');
+const path = require('path');
+const pathToSourceFolder = path.resolve(__dirname, 'files');
 
+fs.mkdir(path.resolve(__dirnme, 'copyDir'));
