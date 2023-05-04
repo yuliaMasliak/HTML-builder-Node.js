ada372ea96c5856ab4ff346a53999f3ae4fbc35e
Yulia Masliak <1985kozak@gmail.com>
2023-05-04 15:34:50 +0300

    feat: add 4 task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,30 @@
-import { copyFile, constants } from 'node:fs/promises';
+const { copyFile, constants } = require('node:fs/promises');
 const fs = require('fs');
 const path = require('path');
+const { stdout, stdin } = process;
 const pathToSourceFolder = path.resolve(__dirname, 'files');
-
-fs.mkdir(path.resolve(__dirnme, 'copyDir'));
+const pathToDestinationFolder = path.resolve(__dirname, 'copyDir');
+function createCopyFiles() {
+  fs.readdir(pathToSourceFolder, (err, data) => {
+    if (err) throw err;
+    data.forEach(async (file) => {
+      const pathToSourceFile = path.resolve(pathToSourceFolder, file);
+      const pathToDestFile = path.resolve(pathToDestinationFolder, file);
+      try {
+        await copyFile(pathToSourceFile, pathToDestFile);
+      } catch (err) {
+        console.log(err.message);
+      }
+    });
+  });
+}
+fs.mkdir(path.resolve(__dirname, 'copyDir'), (err) => {
+  if (err) {
+    createCopyFiles();
+    stdout.write('Files were successfully copied');
+  } else {
+    stdout.write('New directory copyDir was created\n');
+    createCopyFiles();
+    stdout.write('Files were successfully copied');
+  }
+});
