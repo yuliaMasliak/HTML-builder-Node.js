13ae9036ff27b5922a3617224026362e73a0cc99
Yulia Masliak <1985kozak@gmail.com>
2023-05-04 12:08:31 +0300

    feat: add 1 task solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout, stderr, exit } = process;
+const filepath = path.resolve(process.argv[1], 'text.txt');
+const input = fs.createReadStream(filepath);
+let text = '';
+input.on('data', (chunk) => (text += chunk));
+input.on('end', () => {
+  stdout.write(text);
+  exit();
+});
